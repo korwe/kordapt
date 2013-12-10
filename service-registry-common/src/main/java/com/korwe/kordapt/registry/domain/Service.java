f3a71fd3d882e5a05c6ad7115c08cad180ed6edc
Tjad Clark <tjad.clark@korwe.com>
2013-12-10 10:42:13 +0200

    KOR-21: Add shared models - yes duplicated (because of circling dependencies), but should be generated in future

---
+++ service-registry-common/src/main/java/com/korwe/kordapt/registry/domain/Service.java
@@ -0,0 +1,16 @@
+package com.korwe.kordapt.registry.domain;
+
+/**
+ * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
+ */
+public class Service{
+    private String id;
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+}
