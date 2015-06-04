3f375a5b50838ca88734346c64961bbc356bffec
Tjad I Clark <tjataz@gmail.com>
2015-06-04 20:43:46 +0200

    TR-247: Move service-registry to Tree Platform project

--- service-registry/src/main/java/com/korwe/kordapt/registry/dto/ServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package com.korwe.kordapt.registry.dto;
-
-
-/**
- * This file was generated using Kordapt
- */
-public class ServiceProvider {
-
-}
---
+++ test-plugin/src/test/java/com/korwe/test/TestSecurity.java
@@ -0,0 +1,7 @@
+package com.korwe.test;
+
+/**
+ * Created by tjad on 2015/06/01.
+ */
+public class TestSecurity {
+}
