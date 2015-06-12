e95cce9b5df2802668320017dfe33bf3d7165bde
Nithia Govender <nithia.govender@korwe.com>
2015-06-12 10:34:23 +0200

    KOR-33: Create Kordapt-specific GrantedAuthority implementation

---
+++ core/src/main/java/com/korwe/kordapt/acl/KordaptAuthority.java
@@ -0,0 +1,47 @@
+package com.korwe.kordapt.acl;
+
+import com.google.common.base.Objects;
+import org.springframework.security.core.GrantedAuthority;
+
+public class KordaptAuthority implements GrantedAuthority {
+
+    private final String role;
+    private final String serviceGroup;
+
+    public KordaptAuthority(String serviceGroup, String role) {
+        this.serviceGroup = serviceGroup;
+        this.role = role;
+    }
+
+    public String getServiceGroup() {
+        return serviceGroup;
+    }
+
+    @Override public String getAuthority() {
+        return role;
+    }
+
+    @Override public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        final KordaptAuthority that = (KordaptAuthority) o;
+
+        if (!role.equals(that.role)) return false;
+        return serviceGroup.equals(that.serviceGroup);
+
+    }
+
+    @Override public int hashCode() {
+        int result = role.hashCode();
+        result = 31 * result + serviceGroup.hashCode();
+        return result;
+    }
+
+    @Override public String toString() {
+        return Objects.toStringHelper(this)
+                      .add("role", role)
+                      .add("serviceGroup", serviceGroup)
+                      .toString();
+    }
+}
