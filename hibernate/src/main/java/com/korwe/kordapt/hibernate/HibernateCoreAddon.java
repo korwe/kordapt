62ab2f79f9112daa97cacd01a0c60c859112c314
Tjad I Clark <tjataz@gmail.com>
2015-07-15 04:31:27 +0200

    KOR-57: Alias hibernate types stopping class attribute from being present with hibernate specific types

--- hibernate/src/main/java/com/korwe/kordapt/hibernate/HibernateCoreAddon.java
+++ hibernate/src/main/java/com/korwe/kordapt/hibernate/HibernateCoreAddon.java
@@ -3,7 +3,14 @@ package com.korwe.kordapt.hibernate;
 import com.korwe.kordapt.hibernate.xstream.HibernatePersistentCollectionConverter;
 import com.korwe.kordapt.hibernate.xstream.HibernateProxyConverter;
 import com.thoughtworks.xstream.XStream;
+import org.hibernate.collection.internal.PersistentBag;
+import org.hibernate.collection.internal.PersistentList;
+import org.hibernate.collection.internal.PersistentSet;
 import org.springframework.context.support.AbstractApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author <a href="mailto:tjad.clark@korwe.com>Tjad Clark</a>
@@ -21,6 +28,9 @@ public class HibernateCoreAddon implements com.korwe.kordapt.KordaptCoreAddon{
             XStream xStream = (XStream)ctx.getBean("xstream");
             xStream.registerConverter(new HibernateProxyConverter());
             xStream.registerConverter(new HibernatePersistentCollectionConverter(xStream.getMapper()));
+            xStream.addDefaultImplementation(PersistentList.class, List.class);
+            xStream.addDefaultImplementation(PersistentBag.class, List.class);
+            xStream.addDefaultImplementation(PersistentSet.class, Set.class);
         }
 
     }
