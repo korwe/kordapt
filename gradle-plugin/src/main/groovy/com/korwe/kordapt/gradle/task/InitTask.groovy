95887ff833f0bf2c22c5e7266ac5e8029c7ded76
Tjad I Clark <tjataz@gmail.com>
2016-02-29 09:48:27 +0200

    gradle-plugin: Now generate basic client spring configuration

--- gradle-plugin/src/main/groovy/com/korwe/kordapt/gradle/task/InitTask.groovy
+++ gradle-plugin/src/main/groovy/com/korwe/kordapt/gradle/task/InitTask.groovy
@@ -148,6 +148,13 @@ class InitTask extends DefaultTask{
 
             }
 
+                def clientTemplate = springTemplates.getInstanceOf('client')
+                clientTemplate.add('client_name', "${packageName}-Client")
+
+                File clientFile = new File(springDir.absolutePath + File.separator + 'client.xml')
+                clientFile.write(clientTemplate.render())
+
+
         }
 
 
